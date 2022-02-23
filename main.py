d0130ad8b2e77fed055dafa3ccb1c52cddbdf4a6
Vinnubabu425 <81943698+Vinnubabu425@users.noreply.github.com>
2022-02-23 05:38:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pywhatkit
 from pywhatkit.remotekit import send
 
-pywhatkit.sendwhatmsg('+918919182578', "hai bro !, How are you?", 5, 33)
-pywhatkit.sendwhatmsg('+918919182578', "hai bro !, How are you?", 5, 34)
+pywhatkit.sendwhatmsg('+91XXXXXXXXXX', "hai bro !, How are you?", 5, 33)
+pywhatkit.sendwhatmsg('+91XXXXXXXXXX', "hai bro !, How are you?", 5, 34)
 send()
